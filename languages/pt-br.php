762ab59c1961beecc143db58cb621400095f1712
Dmitry <dmitry@pereslegin.ru>
2017-10-22 01:06:42 +0300

    Add pt-br translation file

---
+++ languages/pt-br.php
@@ -0,0 +1,30 @@
+<?php
+
+return [
+    'after %1$s hours'   => [
+        'Depois de %1$s hora',
+        'após %1$s horas'
+    ],
+    'in an hour' => 'em uma hora',
+    'after %1$s minutes' => [
+        'depois de %1$s minuto',
+        'depois de %1$s minutos'
+    ],
+    'in a minute' => 'em um minuto',
+    'in few seconds' => 'em poucos segundos',
+    'now'                => 'agora',
+    'few seconds ago'    => 'alguns segundos atrás',
+    'a minute ago'       => 'um minuto atrás',
+    '%1$s minutes ago'   => [
+        '%1$s minuto atrás',
+        '%1$s minutos atrás'
+    ],
+    'an hour ago'        => 'uma hora atrás',
+    '%1$s hours ago'     => [
+        '%1$s hora atrás',
+        '%1$s horas atrás'
+    ],
+    'yesterday'          => 'ontem',
+    'today'              => 'hoje',
+    'tomorrow'           => 'amanhã',
+];
