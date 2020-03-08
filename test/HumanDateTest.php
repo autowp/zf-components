cb5b68867141700eb821a83003b98947e746513e
Dmitry P <dmitry@pereslegin.ru>
2020-03-08 22:50:08 +0300

    Cover HumanDate

--- test/HumanDateTest.php
+++ test/HumanDateTest.php
@@ -4,7 +4,9 @@ declare(strict_types=1);
 
 namespace AutowpTest\ZFComponents;
 
+use DateInterval;
 use DateTime;
+use Exception;
 use Laminas\Mvc\Application;
 use Laminas\View\Renderer\RendererInterface;
 use PHPUnit\Framework\TestCase;
@@ -20,10 +22,36 @@ class HumanDateTest extends TestCase
         return $serviceManager->get('ViewRenderer');
     }
 
-    public function testHelperWorks(): void
+    /**
+     * @dataProvider periodProvider
+     * @throws Exception
+     */
+    public function testSiblingDates(string $interval, string $expected, bool $invert): void
     {
-        $html = $this->getView()->humanDate(new DateTime());
+        $interval         = new DateInterval($interval);
+        $interval->invert = $invert;
+        $output           = $this->getView()->humanDate((new DateTime())->add($interval));
 
-        $this->assertEquals('today', $html);
+        $this->assertEquals($expected, $output);
+    }
+
+    public static function periodProvider(): array
+    {
+        return [
+            ['PT0M', 'today', false],
+            ['P1D', 'yesterday', true],
+            ['P1D', 'tomorrow', false],
+        ];
+    }
+
+    /**
+     * @throws Exception
+     */
+    public function testDates(): void
+    {
+        $date   = DateTime::createFromFormat(DateTime::ISO8601, '2000-01-01T00:00:00+0200');
+        $output = $this->getView()->humanDate($date);
+
+        $this->assertEquals('January 1, 2000', $output);
     }
 }
