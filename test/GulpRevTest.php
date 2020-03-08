44db054713143c0c5cb975f92a971f86c90318a1
Dmitry P <dmitry@pereslegin.ru>
2020-03-08 23:20:51 +0300

    testAddStylesheet

--- test/GulpRevTest.php
+++ test/GulpRevTest.php
@@ -7,10 +7,20 @@ namespace AutowpTest\ZFComponents;
 use Autowp\ZFComponents\GulpRev;
 use Autowp\ZFComponents\GulpRevException;
 use Laminas\Mvc\Application;
+use Laminas\View\Renderer\RendererInterface;
 use PHPUnit\Framework\TestCase;
 
 class GulpRevTest extends TestCase
 {
+    private function getView(): RendererInterface
+    {
+        $app = Application::init(require __DIR__ . '/_files/config/application.config.php');
+
+        $serviceManager = $app->getServiceManager();
+
+        return $serviceManager->get('ViewRenderer');
+    }
+
     /**
      * @throws GulpRevException
      */
@@ -65,11 +75,7 @@ class GulpRevTest extends TestCase
 
     public function testScriptRevAppends(): void
     {
-        $app = Application::init(require __DIR__ . '/_files/config/application.config.php');
-
-        $serviceManager = $app->getServiceManager();
-
-        $view = $serviceManager->get('ViewRenderer');
+        $view = $this->getView();
 
         $view->gulpRev([
             'scripts' => [
@@ -80,5 +86,20 @@ class GulpRevTest extends TestCase
         $html = $view->headScript()->toString();
 
         $this->assertStringContainsString('&#x2F;test-81bcd394dd.js', $html);
+    }
+
+    public function testAddStylesheet(): void
+    {
+        $view = $this->getView();
+
+        $view->gulpRev([
+            'stylesheets' => [
+                'test.css',
+            ],
+        ]);
+
+        $html = $view->headLink()->toString();
+
+        $this->assertStringContainsString('&#x2F;test-81bcd394dd.css', $html);
     }
 }
